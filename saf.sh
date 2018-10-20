1a04491f2fa71129986e482d873f069172753a2a
agoussia <agoussia@yahoo.com>
2018-10-20 12:23:25 +0300

    Oct 20,  removed accounts

--- saf.sh
+++ saf.sh
@@ -5,42 +5,65 @@
 export PATH=$PATH:$GOPATH/bi
 
 ## util
-protoc  saf/util/ack.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/util/address.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/util/date.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/util/empty.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/util/header.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/util/timestamp.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/util/Ack.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/util/Address.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/util/Date.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/util/Empty.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/util/Header.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/util/Timestamp.proto  -I. --go_out=plugins=grpc:$GOPATH/src
 
 ##base
-protoc  saf/base/engagement.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/base/participant.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/base/transaction.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-
+protoc  saf/base/Engagement.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/base/Owner.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/base/Transaction.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/base/Participant.proto  -I. --go_out=plugins=grpc:$GOPATH/src
 ##shipping
 
-##apps
-protoc  saf/shipping/accounts/shipper.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/accounts/shipping_line.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/accounts/exporter.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/accounts/customs.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/accounts/consignee.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+##participants
+protoc  saf/shipping/participants/Shipper.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/participants/ShippingLine.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/participants/Exporter.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/participants/Customs.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/participants/Consignee.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+
+
+##messages
+protoc  saf/shipping/messages/BookingAcknowledgement.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/messages/CarriageRate.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/messages/EDShipperConfirmation.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/messages/EmptyRelease.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/messages/OriginalBillLading.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/messages/ExportDeclaration.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/messages/ShipperLetterInstruction.proto  -I. --go_out=plugins=grpc:$GOPATH/src
 
 
 ## transactions
-protoc  saf/shipping/transactions/sli/shipper_letter.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/transactions/sli/shipper_letter_instruction_txn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/transactions/eei/ed.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/transactions/eei/ed_txn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/ArrivalNoticeTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/BookingAcknowledgementTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/CarriageRateTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/EDExporterConfirmationTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/EDShipperConfirmationTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/EmptyReleaseTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/EmptyReturnTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/ExportBookingTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/ExportDeclarationTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/FreightPaymentTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/GetCarriageRateTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/GetCarriageRateTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/OriginalBillLadingTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/ShipperLetterInstructionTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/ShippingInstructionTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/transactions/VGMTxn.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+
 
 ## engagements
-protoc  saf/shipping/engagements/cc_ptp_eng.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/engagements/exp_declaration_eng.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/engagements/ContractCarriagePortToPortStraightEng.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/engagements/CustomsExportDeclarationEng.proto  -I. --go_out=plugins=grpc:$GOPATH/src
 
 
 ##services
-protoc  saf/shipping/services/shipper_svc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/services/shipping_line_svc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/services/exporter_svc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/services/customs_svc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
-protoc  saf/shipping/services/consignee_svc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/services/ShipperSvc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/services/ShippingLineSvc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/services/ExporterSvc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/services/CustomsSvc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
+protoc  saf/shipping/services/ConsigneeSvc.proto  -I. --go_out=plugins=grpc:$GOPATH/src
